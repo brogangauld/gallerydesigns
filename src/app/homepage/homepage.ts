83acc6e7ff399e47cb79c138a6d5d90886cccda5
brogangauld <brogangauld97@hotmail.co.uk>
2026-01-24 11:28:39 +0000

    added imports for homepage sections

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { About } from './sections/about/about';
+import { Booking } from './sections/booking/booking';
+import { Contact } from './sections/contact/contact';
+import { Faq } from './sections/faq/faq';
+import { Gallery } from './sections/gallery/gallery';
+import { Hero } from './sections/hero/hero';
+import { HowItWorks } from './sections/how-it-works/how-it-works';
+import { Location } from './sections/location/location';
+import { Reviews } from './sections/reviews/reviews';
+import { Services } from './sections/services/services';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [About, Booking, Contact, Faq, Gallery, Hero, HowItWorks, Location, Reviews, Services],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
   standalone: true
