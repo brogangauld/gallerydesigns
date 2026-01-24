1efe05dc25bfbba5c43e616b00e2cfe3e42d34fe
brogangauld <brogangauld97@hotmail.co.uk>
2026-01-24 11:09:20 +0000

    added footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Homepage } from './homepage/homepage';
 import { Menu } from './menu/menu';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu, Homepage],
+  imports: [RouterOutlet, Menu, Homepage, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
