9c61404b7d47b6b1eac4e5c5146b89746903cb02
brogangauld <brogangauld97@hotmail.co.uk>
2026-01-24 11:07:23 +0000

    added homepage and menu components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Homepage } from './homepage/homepage';
+import { Menu } from './menu/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu, Homepage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
