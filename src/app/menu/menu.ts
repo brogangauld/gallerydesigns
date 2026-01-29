36089c48a65a1816ffaeae3217a1e522ba66303c
brogangauld <brogangauld97@hotmail.co.uk>
2026-01-29 21:33:54 +0000

    updated logo and menu navigation

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,48 +1,40 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { MenubarModule } from 'primeng/menubar';
 import { CommonModule } from '@angular/common';
+import { AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
-  imports: [MenubarModule, CommonModule],
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
   standalone: true
 })
-export class Menu {
+export class Menu implements AfterViewInit {
   items: MenuItem[] | undefined;
+  isOverHero = true;
 
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'Our Work',
-        icon: 'pi pi-image',
-        command: () => { this.scrollTo('gallery') }
+  ngAfterViewInit() {
+    const hero = document.getElementById('hero');
+    if (!hero) return;
+
+    let timeout: any;
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          this.isOverHero = entry.intersectionRatio > 0.3
+        }, 40)
       },
       {
-        label: 'Our Services',
-        icon: 'pi pi-cog',
-        command: () => { this.scrollTo('services') }
-      },
-      // {
-      //   label: 'Our Process',
-      //   icon: 'pi pi-briefcase',
-      //   command: () => { this.scrollTo('how-it-works') }
-      // },
-      {
-        label: 'Materials',
-        icon: 'pi pi-book',
-        disabled: true,
-        // command: () => { this.scrollTo('how-it-works')}
-      },
-      {
-        label: 'Contact Us',
-        icon: 'pi pi-envelope',
-        command: () => { this.scrollTo('contact') }
+        threshold: [0, 0.15, 0.3],
+        rootMargin: '-120px 0px 0px 0px'
       }
-    ];
+    );
+
+    observer.observe(hero);
   }
+
   scrollTo(anchorId: string) {
     const el = document.getElementById(anchorId);
     if (el) {
