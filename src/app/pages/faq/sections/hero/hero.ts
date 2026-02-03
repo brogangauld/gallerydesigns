d11942c7787646f6e3339ee4f9a0dbe5ae7b9d86
brogangauld <brogangauld97@hotmail.co.uk>
2026-02-03 22:23:52 +0000

    added faq hero component and img file

---
+++ src/app/pages/faq/sections/hero/hero.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TagModule } from 'primeng/tag';
+
+@Component({
+  selector: 'app-hero',
+  imports: [TagModule],
+  templateUrl: './hero.html',
+  styleUrl: './hero.css',
+})
+export class Hero {
+
+}
